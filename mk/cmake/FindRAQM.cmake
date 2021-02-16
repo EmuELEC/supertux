9b45398d1e18b92830c5bac566aa3f6f8f2665d1
shantigilbert <shantic@gmail.com>
2021-02-15 20:09:03 -0600

    data_only

--- mk/cmake/FindRAQM.cmake
+++
@@ -1,8 +0,0 @@
-include(FindPkgConfig)
-pkg_check_modules(PC_RAQM raqm)
-find_path(RAQM_INCLUDE_DIR NAMES raqm.h HINTS ${PC_RAQM_INCLUDE_DIRS}
-  ${PC_RAQM_INCLUDEDIR})
-find_library(RAQM_LIBRARY NAMES raqm HINTS ${PC_RAQM_LIBRARY_DIRS}
-  ${PC_RAQM_LIBDIR})
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RAQM DEFAULT_MSG RAQM_INCLUDE_DIR RAQM_LIBRARY)
